888ba9f2c8b1a1bacb822c67ca4945dafa81ec8e
Edwin-Salazar <138740962+Edwin-Salazar@users.noreply.github.com>
2024-06-29 22:02:57 -0700

    Updates

---
+++ FE12/Capacity Estimation Model/AC_Capacity_Estimation_Model_Parameters.m
@@ -0,0 +1,9 @@
+
+%%Vehicle Parameters
+vehicleMass = 284; %Kilograms(kg)
+g = 9.8; %Acceleration due to gravity (m/s^2)
+angle = 0; %Slope angle (rad)
+rollingResistCoeff = 0.030; 
+airDensity = 1.165; %Kg/m^3 (30deg C)
+airDragCoeff = 0.558; 
+frontalArea = 0.958; %m^2
