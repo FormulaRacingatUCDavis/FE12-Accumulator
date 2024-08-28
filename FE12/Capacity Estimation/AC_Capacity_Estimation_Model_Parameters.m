6c5ddd01200f9d572a5aec0eb1d0503476bc2578
Edwin-Salazar <138740962+Edwin-Salazar@users.noreply.github.com>
2024-08-27 17:06:57 -0700

    update

--- FE12/Capacity Estimation/AC_Capacity_Estimation_Model_Parameters.m
+++ FE12/Capacity Estimation/AC_Capacity_Estimation_Model_Parameters.m
@@ -1,9 +1,9 @@
 
 %%FE11 Vehicle Parameters
-vehicleMass = 284; %Kilograms(kg) + 140 lbs driver 
+vehicleMass = 284; % (284 Kilogram = FE11 Car + 140 lbs driver) + 20 kg aero
 g = 9.8; %Acceleration due to gravity (m/s^2)
 angle = 0; %Slope angle (rad)
 rollingResistCoeff = 0.030; 
 airDensity = 1.165; %Kg/m^3 (30deg C)
 airDragCoeff = 0.558; 
-frontalArea = 0.958; %m^2
+frontalArea = 0.6; %m^2
