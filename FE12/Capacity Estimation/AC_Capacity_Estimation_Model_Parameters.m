6d85be456a69226507f34d7ccd6a4d9ebf02afe0
aimeetieu9 <aatieu@ucdavis.edu>
2024-09-01 14:14:21 -0700

    Update AC_Capacity_Estimation_Model_Parameters.m

--- FE12/Capacity Estimation/AC_Capacity_Estimation_Model_Parameters.m
+++ FE12/Capacity Estimation/AC_Capacity_Estimation_Model_Parameters.m
@@ -1,9 +1,9 @@
 
 %%FE11 Vehicle Parameters
-vehicleMass = 284; % (284 Kilogram = FE11 Car + 140 lbs driver) + 20 kg aero
+vehicleMass = 305+9; % (284 Kilogram = FE11 Car + 140 lbs driver) + 20 kg aero
 g = 9.8; %Acceleration due to gravity (m/s^2)
 angle = 0; %Slope angle (rad)
 rollingResistCoeff = 0.030; 
 airDensity = 1.165; %Kg/m^3 (30deg C)
 airDragCoeff = 0.558; 
-frontalArea = 0.6; %m^2
+frontalArea = 0.455 + 0.5; %m^2
