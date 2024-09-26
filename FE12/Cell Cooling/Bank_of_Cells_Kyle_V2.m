87355b1f0cabc2af156b12bc023f8c28e56140b7
Kyle4980 <115690504+Kyle4980@users.noreply.github.com>
2024-09-26 14:01:44 -0700

    Update Bank of Cells

--- FE12/Cell Cooling/Bank_of_Cells_Kyle_V2.m
+++ FE12/Cell Cooling/Bank_of_Cells_Kyle_V2.m
@@ -1,6 +1,6 @@
 %% Bank of Tubes Analysis on Subpack Cooling
 clear all 
-close all
+% close all
 %% Assumptions
 %Nusselt Number correlation does not technically hold true since our flow
 %regieme changes as a function of our velocity, however it is a decent
@@ -17,10 +17,9 @@ P_max = 20; % [kW] Max power
 
 %Cell Spacing (from center of one cell to another)
 % s_t = 23*10^-3; % [m] traverse spacing
-% s_l = 24*10^-3; % [m] longitudinal spacing
-s_l = 23*10^-3 : 0.5e-3 : 24e-3; % [m] traverse spacing
-s_t = 2e-3; % [m] longitudinal spacing
-%s_l = 23*10^-3 : 0.5e-3 : 27e-3; % [m] longitudinal spacing
+s_l = 24*10^-3; % [m] longitudinal spacing
+s_t = 23e-3; 
+%s_t = 23e-3: 0.5e-3 : 24e-3; % [m] longitudinal spacing
 %s_d = sqrt(s_l^2 + s_t^2); %[m] diagonal spacing
 
 %Constants Based on Cell Config 
@@ -28,35 +27,46 @@ s_t = 2e-3; % [m] longitudinal spacing
 %C1 = 0.9;
 C2 = 0.95;
 m = 0.6;
-%m = 0.4
 
 %Cell Properties
-T_cell_max = 50; %[C] 
+T_cell_max = 55; %[C] 
 T_ambient = 35; %[C]
 N_rows = 12; % Number of rows
 N_cells = 6; % Number of cells per row
 totalcells = N_rows*N_cells;
 D_cell = 21.55 *10^-3; %[m] Cell diameter
 R_cell = D_cell/2; % [m] Cell radius
-%R_cell = 12.93e-3; % this is gerater than radius of the cell, larger
-%surface area = lower pressure drop
 k_cell = 2.21; %[W /m K] 
 L_cell = 70.15*10^-3; % [m] Cell length/height
 SA_cell = 0.5*(L_cell-12.7*10^-3)*2*pi*R_cell; %[m^2] Surface Area of cell
 R_internal = 0.0087; % [Ohms]
 
-%Air Properties at 30 C
-rho = 1.164; % [kg/m^3]
-Cp = 1006; % [J/kg K]
-mu = 18.72*10^-6; % [N s/m^2]
-k = 26.62*10^-3; % [W/m K] 
+% %Air Properties at 30 C
+% rho = 1.164; % [kg/m^3]
+% Cp = 1006; % [J/kg K]
+% mu = 18.72*10^-6; % [N s/m^2]
+% k = 26.62*10^-3; % [W/m K] 
+% Pr = mu*Cp/k; %
+
+%Air Properties at 35 C
+rho = 1.1459; % [kg/m^3]
+Cp = 1006.7; % [J/kg K]
+mu = 18.915*10^-6; % [N s/m^2]
+k = 26.71*10^-3; % [W/m K] 
 Pr = mu*Cp/k; %
 
-%Air Properties at 50 C (Surface Temp)
-rho_s = 1.093; % [kg/m^3]
-Cp_s = 1007; % [J/kg K]
-mu_s = 19.53*10^-6; % [N s/m^2]
-k_s = 28.08*10^-3; % [W/m K]
+% %Air Properties at 50 C (Surface Temp)
+% rho_s = 1.093; % [kg/m^3]
+% Cp_s = 1007; % [J/kg K]
+% mu_s = 19.53*10^-6; % [N s/m^2]
+% k_s = 28.08*10^-3; % [W/m K]
+% Pr_s = mu_s*Cp_s/k_s; 
+
+%Air Properties at 55 C (Surface Temp)
+rho_s = 1.0759; % [kg/m^3]
+Cp_s = 1007.7; % [J/kg K]
+mu_s = 19.835*10^-6; % [N s/m^2]
+k_s = 28.16*10^-3; % [W/m K]
 Pr_s = mu_s*Cp_s/k_s; %
 
 
@@ -114,14 +124,10 @@ for j = 1:length(s_t)
         P_ratio = P_t/P_l;
 
         %Rough estimates of pressure drop coefficient
-        X=1.1; % staggered graph
+        X= 1.1; % staggered graph
         f = 0.9; % staggered graph
-        f = 0.9;
         P_delta(j,i) = N_rows*X*(rho*V_max^2/2)*f; %[Pa]
     
-        Ts_minus_To(j,i) = (T_cell_max-T_ambient)*exp((-pi*D_cell/1000*totalcells*h)/(rho*V_inlet(i)*N_cells*s_t(j)/1000*Cp*1000));
-        logmeantempdiff(j,i) = (T_cell_max-T_ambient-Ts_minus_To(i))/log((T_cell_max-T_ambient)/Ts_minus_To(i)); %degC, difference in inlet and outlet air temp
-        heattransferrate(j,i)= totalcells*h*pi*D_cell*logmeantempdiff(i)/1000; %kW/m
     end
 end
 
